b260ab7de4f98fe130ad302ee0aaf5e9f7b8f076
VitorNunes86 <145569863+VitorNunes86@users.noreply.github.com>
2025-09-30 12:06:10 -0300

    30/09

--- main.js
+++ main.js
@@ -18,54 +18,54 @@ const perguntas = [
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: "Na sua opinião, as redes sociais e plataformas digitais ajudam ou dificultam o acesso a informações confiáveis?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "As redes sociais dificultam o acesso a informações confiáveis, devido à propagação de fake news e desinformação.",
+                afirmacao: "As redes sociais dificultam o acesso a informações confiáveis, pois a propagação de fake news e desinformação muitas vezes ofusca o conteúdo legítimo, criando um ambiente de confusão e desconfiança",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "As redes sociais oferecem oportunidades de acesso a informações relevantes e rápidas, mas exigem discernimento para filtrar o que é verdadeiro.",
+                afirmacao: "Embora as redes sociais ofereçam uma abundância de informações, é necessário discernir corretamente o que é verdadeiro, tornando-se essencial o desenvolvimento da capacidade crítica do usuário para filtrar as fontes confiáveis.",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: "Você acha que a coleta de dados pessoais na internet é um risco para a privacidade?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Sim, a coleta de dados pessoais representa um risco considerável para a privacidade e pode ser usada de maneira indevida.",
+                afirmacao: " A coleta massiva de dados pessoais na internet representa um risco significativo à privacidade dos usuários, uma vez que esses dados podem ser utilizados indevidamente por empresas ou indivíduos com más intenções.",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Não, desde que as empresas sigam normas de proteção de dados, os riscos podem ser minimizados, e a coleta pode ser benéfica",
+                afirmacao: "A coleta de dados pessoais não é um risco, desde que as empresas sigam normas de segurança e proteção de dados, garantindo que os usuários sejam informados e que suas informações sejam tratadas com transparência.",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: "Você acredita que a dependência de tecnologias e redes sociais pode prejudicar a produtividade e as relações pessoais?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Sim, a dependência digital pode causar vícios e diminuir o tempo dedicado a atividades produtivas e relacionamentos reais.",
+                afirmacao: "A dependência de tecnologias e redes sociais pode diminuir a produtividade e prejudicar as relações pessoais, pois muitas vezes as pessoas acabam se isolando e deixando de interagir de forma significativa no mundo real.",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Não, acredito que as tecnologias podem ser usadas de forma equilibrada, ajudando na produtividade e facilitando o contato com amigos e familiares.",
+                afirmacao: "As tecnologias e redes sociais, quando usadas de forma equilibrada, podem aumentar a produtividade e facilitar a comunicação, mantendo as pessoas conectadas com familiares, amigos e colegas de trabalho.",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: "Na sua visão, a desigualdade no acesso à tecnologia é um problema importante no contexto digital?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: "Afirmação",
+                texto: "Sim, a desigualdade digital impede que muitas pessoas tenham as mesmas oportunidades educacionais e profissionais.",
+                afirmacao: " A desigualdade digital é um problema crítico, pois impede que muitas pessoas, especialmente em regiões carentes, tenham acesso a oportunidades educacionais, profissionais e sociais, exacerbando a exclusão social.",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Não, acredito que com o tempo a tecnologia se tornará acessível a todos, diminuindo a desigualdade digital.",
+                afirmacao: "Embora a desigualdade digital seja uma preocupação, acredito que a tecnologia se tornará cada vez mais acessível, e em breve todos terão as mesmas oportunidades de participar da revolução digital.",
             }
         ]
     },
