371bdf82ade174c256e539829369d6025d1e32b9
VitorNunes86 <145569863+VitorNunes86@users.noreply.github.com>
2025-09-30 11:45:49 -0300

    30

--- main.js
+++ main.js
@@ -5,15 +5,15 @@
 
 const perguntas = [
     {
-        enunciado: "Pergunta?",
+        enunciado: "Você acredita que o isolamento social gerado pela interação virtual pode afetar a saúde emocional?",
         alternativas: [
             {
-                texto: " Texto/resposta",
-                afirmacao: " Afirmação ",
+                texto: "Sim, acredito que a falta de interações presenciais pode levar ao aumento da solidão e de problemas emocionais.",
+                afirmacao: "O isolamento social causado pela interação virtual pode resultar em um aumento de sentimentos de solidão e ansiedade.",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Não, acho que as interações online ainda permitem conexões suficientes para manter o equilíbrio emocional.",
+                afirmacao: "Permitem conexões suficientes para manter o equilíbrio emocional, desde que o uso seja equilibrado.",
             }
         ]
     },
